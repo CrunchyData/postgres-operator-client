fe8a7649d41f5710587876299579d10ee2c5b162
Chris Bandy <chris.bandy@crunchydata.com>
2022-06-09 10:48:12 -0500

    Initial root command and Make targets
    Issue: [sc-14638]
    Co-authored-by: Jonathan S. Katz <jonathan.katz@crunchydata.com>

---
+++ cmd/kubectl-pgo/main.go
@@ -0,0 +1,33 @@
+// Copyright 2021 - 2022 Crunchy Data Solutions, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+
+	"github.com/crunchydata/postgres-operator-client/internal/cmd"
+)
+
+func main() {
+	flags := pflag.NewFlagSet("kubectl-pgo", pflag.ExitOnError)
+	pflag.CommandLine = flags
+
+	root := cmd.NewCmdPGO(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+	cobra.CheckErr(root.Execute())
+}
