03abda41de2165c1842ddf171260bde0c80b0f66
Chris Bandy <chris.bandy@crunchydata.com>
2022-07-15 19:32:38 -0500

    Unstructured PostgresCluster client

---
+++ internal/apis/postgres-operator.crunchydata.com/v1beta1/postgrescluster_types.go
@@ -0,0 +1,49 @@
+// Copyright 2021 - 2022 Crunchy Data Solutions, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/dynamic"
+)
+
+func NewPostgresClusterClient(rcg resource.RESTClientGetter) (
+	*meta.RESTMapping, dynamic.NamespaceableResourceInterface, error,
+) {
+	gvk := GroupVersion.WithKind("PostgresCluster")
+
+	mapper, err := rcg.ToRESTMapper()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	config, err := rcg.ToRESTConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return mapping, client.Resource(mapping.Resource), nil
+}
